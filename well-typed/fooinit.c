b88f012acbbe1e74dba80c417436cb5928df97f8
Jason Dusek <jason.dusek@gmail.com>
2011-03-13 01:18:01 +0000

    Even simpler.

--- well-typed/fooinit.c
+++ well-typed/fooinit.c
@@ -4,9 +4,9 @@ extern void __stginit_Foo(void);
 static void Foo_init (void) __attribute__ ((constructor));
 void Foo_init (void) {
   int argc = 1;
-  char *argv[] = {""};
-  char **argv_p = argv;
-  hs_init(&argc, &argv_p);
+  char *arg = "";
+  char **argv = &arg;
+  hs_init(&argc, &argv);
   hs_add_root(__stginit_Foo);
 }
 
