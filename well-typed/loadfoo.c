48e047e0abd56f1213ce314bdfe0e38ae76e054e
Jason Dusek <jason.dusek@gmail.com>
2011-03-13 21:50:52 +0000

    Clean up loadfoo; nicer C.

--- well-typed/loadfoo.c
+++ well-typed/loadfoo.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dlfcn.h>
 
+void dldie(void) {
+  char* e = dlerror();
+  if ( e != NULL ) {
+    printf("**** %s: %s\n", "dynamic loading error", e);
+    exit(EXIT_FAILURE);
+  }
+}
+
 int main(int argc, char* argv[]) {
-  char* e;
   char* so = argv[1];
   printf("**** %s %s\n", "trying to load", so);
-
   void *dl = dlopen(so, RTLD_NOW);
-  e = dlerror();
-  if ( e == NULL ) {
-    printf("**** %s\n", ".so load okay");
-    int (*foo)(int) = dlsym(dl, "foo");
-    e = dlerror();
-    if ( e == NULL ) {
-      printf("**** %s\n", "symbol lookup okay");
-      printf("**** foo(7) -> %d\n", foo(7));
-    } else {
-      printf("**** %s: %s\n", "symbol lookup error", e);
-    }
-  } else {
-    printf("**** %s: %s\n", ".so load error", e);
-  }
-
-  return 0;
+  dldie();
+  printf("**** %s\n", ".so load okay");
+  int (*foo)(int) = dlsym(dl, "foo");
+  dldie();
+  printf("**** %s\n", "symbol lookup okay");
+  printf("**** foo(7) -> %d\n", foo(7));
+  exit(EXIT_SUCCESS);
 }
 
