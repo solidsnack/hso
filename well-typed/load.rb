efb3e89409e4811bf3ea9ad63a7ed155c16939cf
Jason Dusek <jason.dusek@gmail.com>
2011-04-21 02:19:44 +0000

    Fixed to it loads the dynamic one.

--- well-typed/load.rb
+++ well-typed/load.rb
@@ -3,7 +3,7 @@ require 'dl/import'
 
 module Foo
 extend DL::Importable
-  DL_PATH = File.dirname(__FILE__) + '/libfoo.static-static.so'
+  DL_PATH = File.dirname(__FILE__) + '/libfoo.dynamic-dynamic.so'
   dlload(DL_PATH)
   extern 'int foo(int)'
 end
